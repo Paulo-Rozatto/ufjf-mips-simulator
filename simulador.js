2a91f75134e4e11df9d00cb45bb2054d7345abc8
paulo-rozatto <rozatto.paulo.victor@gmail.com>
2022-01-27 22:49:03 -0300

    first and second half cycle

--- simulador.js
+++ simulador.js
@@ -1,3 +1,6 @@
+const FIRST_HALF = 0;
+const SECOND_HALF = 1;
+
 // Criacao de array tipado em javascript, estao sendo alocados 128 bytes para corresponder os 32 registradores de 32 bits
 const registers = new Int32Array(new ArrayBuffer(128));
 
@@ -42,80 +45,149 @@ function loadFromTextArea() {
 loadFromTextArea();
 
 function cycle() {
-    instruction_fetch();
-    instruction_decode();
-    execute();
-    memory_read();
-    write_back();
+    console.log("Primeira metade")
+
+    instruction_fetch(FIRST_HALF);
+    instruction_decode(FIRST_HALF);
+    execute(FIRST_HALF);
+    memory_read(FIRST_HALF);
+    write_back(FIRST_HALF);
+
+    console.log("Segunda metade")
+    instruction_fetch(SECOND_HALF);
+    instruction_decode(SECOND_HALF);
+    execute(SECOND_HALF);
+    memory_read(SECOND_HALF);
+    write_back(SECOND_HALF);
 }
-cycle();
+
+for (let i = 1; i <= 5; i++) {
+    console.log("ciclo " + i);
+    cycle();
+}
 
 // todo: trocar nome das funcoes
-function instruction_fetch() { // Busca instrucao
+function instruction_fetch(half) { // Busca instrucao
     // console.log('IF')
     // console.log(`pc: ${pc}, inst: ${instruction_memory[pc]}`)
 
-    if_id[0] = instruction_memory[pc / 4]; // o array aloca 1 byte em cada posicao e o pc esta em bytes
-    pc += 4;
-    if_id[1] = pc;
+    console.log("Instruction fetch");
+    if (half === FIRST_HALF) { // primeira metada de ciclo apenas leitura
 
-    htmlWrite('pc', pc);
+        this.instruction = instruction_memory[pc / 4]; // o array aloca 1 byte em cada posicao e o pc esta em bytes
+        this.pcIncremented = pc + 4;
+
+        console.log('instruction: ' + (this.instruction >>> 0).toString(2).padStart(32, '0'))
+        console.log('next pc', this.pcIncremented);
+    }
+    else if (half == SECOND_HALF) {
+        if_id[0] = this.instruction;
+        if_id[1] = this.pcIncremented;
+
+        // console.log("pc: ", pc);
+        pc = this.pcIncremented;
+        console.log(if_id);
+        htmlWrite('pc', pc);
+    }
 
     // console.log(if_id);
 }
 
-function instruction_decode() { // Decodifica instrucoes
-    let rs = (if_id[0] >>> 21) & 0b00000000000000000000000000011111; // [25-21]
-    let rt = (if_id[0] >>> 15) & 0b00000000000000000000000000011111; // [20-16]
-    let imediate = if_id[0] & 0b00000000000000001111111111111111; // [15 - 0]
+function instruction_decode(half) { // Decodifica instrucoes
+    console.log("Instruction decode")
 
-    // extender o sinal: zero fill para a esquerda e depois  sigend shift para a direita
-    imediate = (imediate << 16) >> 16;
+    if (half == FIRST_HALF) {
+        this.rs = (if_id[0] >>> 21) & 0b00000000000000000000000000011111; // [25-21]
+        this.rt = (if_id[0] >>> 15) & 0b00000000000000000000000000011111; // [20-16]
+        this.imediate = if_id[0] & 0b00000000000000001111111111111111; // [15 - 0]
+        this.nextPc = if_id[1];
 
-    id_ex[0] = registers[rs];
-    id_ex[1] = registers[rt]
-    id_ex[2] = imediate;
-    id_ex[3] = if_id[1]; // salva PC + 4
-    id_ex[4] = rt // salva endereco para escrever load word
+        // extender o sinal: zero fill para a esquerda e depois  sigend shift para a direita
+        imediate = (imediate << 16) >> 16;
 
+        console.log({
+            rs: this.rs,
+            rt: this.rt,
+            imediate: this.imediate,
+            nextPc: this.nextPc
+        })
 
-    control.set(if_id[0]);
-    // console.log(rs, rt);
-    // console.log(id_ex);
+        control.set(if_id[0]);
+    }
+    else if (half == SECOND_HALF) {
+        id_ex[0] = registers[this.rs];
+        id_ex[1] = registers[this.rt];
+        id_ex[2] = this.imediate;
+        id_ex[3] = this.nextPc; // salva PC + 4
+        id_ex[4] = this.rt; // salva endereco para escrever load word
+
+        console.log(id_ex);
+        // console.log(control)
+        // console.log(rs, rt);
+        // console.log(id_ex);
+    }
 }
 
-function execute() { // execucao ou calculo de endereco
+function execute(half) { // execucao ou calculo de endereco
+    console.log("Execute")
 
-    let result = alu.execute(0b10, id_ex[0], id_ex[2]) // rs + imediate
+    if (half == FIRST_HALF) {
+        this.result = alu.execute(0b10, id_ex[0], id_ex[2]) // rs + imediate
+        console.log({ result: this.result });
+    }
+    else if (half == SECOND_HALF) {
+        ex_mem[0] = this.result;
+        ex_mem[3] = id_ex[4]; // endereco registrador de escrita load word
 
-    ex_mem[0] = result;
-    ex_mem[3] = id_ex[4]; // endereco registrador de escrita load word
-
-    console.log(result);
+        console.log(ex_mem);
+    }
 }
 
-function memory_read() { // acesso a memoria
-    data_memory[1] = 0b00000000000000000000000010000010
+function memory_read(half) { // acesso a memoria
+    console.log("Memory read");
 
-    // data_memory guarda words, entao precisa dividir o endereco por 4
-    let address = Math.floor(ex_mem[0] / 4);
+    if (half == FIRST_HALF) {
+        data_memory[1] = 0b00000000000000000000000010000010
 
-    console.log('memory read')
-    console.log(address, data_memory[address])
+        // data_memory guarda words, entao precisa dividir o endereco por 4
+        this.address = Math.floor(ex_mem[0] / 4);
+        this.regAddres = ex_mem[3];
 
-    mem_wb[0] = data_memory[address];
-    mem_wb[1] = ex_mem[3];
+        console.log({
+            address: this.address,
+            regAddress: this.regAddres
+        })
+    }
+    else if (half == SECOND_HALF) {
+        mem_wb[0] = data_memory[this.address];
+        mem_wb[1] = this.regAddres;
 
+        console.log(mem_wb);
+    }
 }
 
-function write_back() { // escrita do resultado
-    registers[mem_wb[1]] = mem_wb[0]; // escreve no registrador guardade em mem_wb[1] o valor lido da memoria guardado em mem_wb[0]
+function write_back(half) { // escrita do resultado
+    console.log("Write back")
 
-    console.log('write back')
-    console.log(mem_wb[1].toString(2), registers[mem_wb[1]])
+    if (half == FIRST_HALF) {
+        this.dst = mem_wb[1];
+        this.value = mem_wb[0];
 
+        console.log({
+            dst: this.dst,
+            value: this.value
+        })
+    }
+    else if (half == SECOND_HALF) {
+        registers[this.dst] = this.value;
 
-    updateUI();
+        // registers[mem_wb[1]] = mem_wb[0]; // escreve no registrador guardade em mem_wb[1] o valor lido da memoria guardado em mem_wb[0]
+        console.log({
+            register: registers[this.dst]
+        })
+
+        updateUI();
+    }
 }
 
 function updateUI() {
